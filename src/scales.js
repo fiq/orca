ac9cf93f443018afca9a98edffb193f8577afdb3
Raf Gemmail <rafiq@dreamthought.com>
2025-01-03 00:36:15 +1300

    fix - fixed semitone and flats bug

--- src/scales.js
+++ src/scales.js
@@ -2,55 +2,51 @@ const scales = {
 	"ionian":    ['W', 'W', 'H', 'W', 'W', 'W', 'H'],
 	"dorian":    ['W', 'H', 'W', 'W', 'W', 'H', 'W'],
 	"phyrigian": ['H', 'W', 'W', 'W', 'H', 'W', 'W'],
-	"lydian":    ['H', 'W', 'W', 'W', 'H', 'W', 'W'],
+	"lydian":    ['W', 'W', 'W', 'H', 'W', 'W', 'H'],
 	"mixolydian":['W', 'W', 'H', 'W', 'W', 'H', 'W'],
 	"aeolian":   ['W', 'H', 'W', 'W', 'H', 'W', 'W'],
 	"locrian":   ['H', 'W', 'W', 'H', 'W', 'W', 'W']
 };
 
-const MINOR_SYMBOL = 'b';
-
-const offset_from_a = (note) => {
-  const note_char = note.charCodeAt(0);
-  return note_char - 65;
-};
+const OCTAVE = ['Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G'];
 
 export const get_scale = (root, type) => {
+  // get octave and details
+  const note_details = get_note_and_octave(root);
+
+  const [startIndex] = OCTAVE.map( (note, idx) => { if (note === note_details.note) return idx; } ).filter( x=>x );
   const result = [root];
   const scale_formula = [...scales[type]];
-  return build_scale(result, scale_formula);
+  return build_scale(result, startIndex, scale_formula);
 };
 
 const get_note_and_octave = (full_note) => {
-  const matches = full_note.match(/(?<note>\D+?)(?<isminor>b?)(?<octave>\d+)/).groups;
+  const matches = full_note.match(/(?<note>\D+?)(?<isflat>b?)(?<octave>\d+)/).groups;
   return {
 	  note: matches.note,
-	  is_minor: matches.isminor,
-	  octave:  parseInt(matches.octave),
-	  offset_from_a: offset_from_a(matches.note)
+	  is_flat: matches.isflat,
+	  octave:  parseInt(matches.octave)
   };
 };
 
-const get_next_note = (last_note, step) => {
-  const offset_from_a = (last_note.offset_from_a + 1) % 7;
-  const octave = offset_from_a < last_note.offset_from_a ? last_note.octave + 1 : last_note.octave;
-  const note = String.fromCharCode(offset_from_a + 65);
-  const isminor = step === 'H' ? 'b' : '';
-
-  return `${note}${isminor}${octave}`;
-}
-
-const build_scale = (result, scale_formula) => {
-  // decompose note
-  const last_note = get_note_and_octave(result[result.length-1]);
-
+const build_scale = (result, idx, scale_formula) => {
+  // get octave and details
+  const note_details = get_note_and_octave(result[result.length-1]);
   // use scale to determine if flat
   const step = scale_formula.shift();
-  result.push(get_next_note(last_note, step));
-   
+  let idx_next = idx;
+  switch (step) {
+    case "H": idx_next = (idx+1) % OCTAVE.length ; break;
+    default: idx_next = (idx+2) % OCTAVE.length;  break;
+  }
+
+  const octave = (idx_next < idx) ? note_details.octave+1 : note_details.octave;
+  const next_note = OCTAVE[idx_next];
+  result.push(`${next_note}${octave}`);
+  
   // recurse
   if (scale_formula.length === 0) {
 	  return result;
   } 
-  return build_scale(result, scale_formula);
+  return build_scale(result, idx_next, scale_formula);
 }
